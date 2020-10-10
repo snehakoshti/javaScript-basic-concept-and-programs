8f6aba2a5c841c8cf736e97a8de5f67b6d7e06f8
shadabshaikh0 <shadab.shaikh@whitehatjr.com>
2020-10-10 09:48:43 +0530

    Added shift unshift operation for multidimensional array

--- shifUnshiftOnArray.js
+++ shifUnshiftOnArray.js
@@ -2,9 +2,23 @@ var arr=[2,3,4];
 //enter 5 at first
 arr.unshift(5);
 console.log(arr);  // [5, 2, 3, 4]
-//enter 6 at end
+//enter 6 at first
 arr.unshift(6);
 console.log(arr);  // [6, 5, 2, 3, 4]
 //remove first element
 arr.shift();
 console.log(arr);  // [5, 2, 3, 4]
+
+var multiDimensionalArray = [
+    [1,2,3],
+    [4,5,6]
+]
+console.log(multiDimensionalArray);  // [ [1,2,3], [4,5,6] ]
+
+//enter array [0,0,0] at first
+multiDimensionalArray.unshift([0,0,0])
+console.log(multiDimensionalArray);  // [ [0,0,0], [1,2,3], [4,5,6] ]
+
+//remove first array [0,0,0]
+multiDimensionalArray.shift()
+console.log(multiDimensionalArray);  // [ [1,2,3], [4,5,6] ]
