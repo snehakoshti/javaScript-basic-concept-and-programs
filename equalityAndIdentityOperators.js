810553c6150152474e596cb961ade6028b501288
Andres Ruiz <andrsruiz@gmail.com>
2020-10-11 09:34:48 -0500

    Add more explanation

--- equalityAndIdentityOperators.js
+++ equalityAndIdentityOperators.js
@@ -2,9 +2,11 @@ let threeNumber = 3
 let threeString = '3'
 
 /* Equaly operator does do type conversion before the comparison. */
+console.log('3 == "3"')
 if(threeNumber == threeString)
 	console.log('3 and "3" are the same')
 
+console.log('3 === "3"')
 /* Identity operator doesn't do any type conversion. */
-if(threeNumber === threeString)
-	console.log('3 and "3 are not the same"')
+if(!(threeNumber === threeString))
+	console.log('3 and "3" are not the same')
